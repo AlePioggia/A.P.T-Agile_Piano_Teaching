c75ab918f119b3a2aa865156fa9d998376f8b151
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2022-07-08 16:56:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/apt_agile_piano_teaching/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.apt_agile_piano_teaching;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
