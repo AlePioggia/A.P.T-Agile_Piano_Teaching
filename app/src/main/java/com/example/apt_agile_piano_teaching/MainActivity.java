1115218cc81c2c31bba52b98c1b7ca4dc624ce46
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2022-07-11 15:41:53 +0200

    Add menu items

--- app/src/main/java/com/example/apt_agile_piano_teaching/MainActivity.java
+++ app/src/main/java/com/example/apt_agile_piano_teaching/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.apt_agile_piano_teaching;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.apt_agile_piano_teaching.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+         setContentView(binding.getRoot());
+        this.replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    return true;
+                case R.id.add:
+                    replaceFragment(new AddFragment());
+                    return true;
+                case R.id.menu:
+                    replaceFragment(new MenuFragment());
+                    return true;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    return true;
+            }
+
+            return false;
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
