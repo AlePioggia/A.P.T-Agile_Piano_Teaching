3a73ce82c07f347692bdc64425260d10a77d5c5c
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2022-07-15 18:28:45 +0200

    add method that makes me able to retrieve images

--- app/src/main/java/com/example/apt_agile_piano_teaching/ProfileFragment.java
+++ app/src/main/java/com/example/apt_agile_piano_teaching/ProfileFragment.java
@@ -1,20 +1,35 @@
 package com.example.apt_agile_piano_teaching;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.io.Console;
+import java.io.File;
 
 
 /**
@@ -25,7 +40,9 @@ import com.google.firebase.storage.StorageReference;
 public class ProfileFragment extends Fragment {
 
     private FirebaseUser user;
-
+    private StorageReference mStorageRef;
+    private ImageView mProfileImageShow;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -51,10 +68,15 @@ public class ProfileFragment extends Fragment {
         TextView surname = view.findViewById(R.id.surname);
         TextView mail = view.findViewById(R.id.mail);
         Button logoutButton = view.findViewById(R.id.logoutButton);
+        mProfileImageShow = view.findViewById(R.id.showProfileImage);
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads/" + mAuth.getCurrentUser().getEmail() + ".jpg");
+        Glide.with(getActivity()).load(mStorageRef).into(mProfileImageShow);
 
         firstName.setText(" empty name ");
         surname.setText(" empty surname ");
         mail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+
+        StorageReference imageRef = mStorageRef.child("uploads/alexpioggia@gmail.com.jpg");
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
