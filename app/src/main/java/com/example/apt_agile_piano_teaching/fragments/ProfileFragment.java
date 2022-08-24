bd1ca30513c96a36d12e8ad60a9906d872b6e9cc
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2022-08-24 17:10:49 +0200

    add edit profile

--- app/src/main/java/com/example/apt_agile_piano_teaching/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/apt_agile_piano_teaching/fragments/ProfileFragment.java
@@ -1,6 +1,9 @@
 package com.example.apt_agile_piano_teaching.fragments;
 
+import android.app.Dialog;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,17 +12,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.apt_agile_piano_teaching.activities.EditLessonsActivity;
 import com.example.apt_agile_piano_teaching.activities.ImagesActivity;
 import com.example.apt_agile_piano_teaching.activities.LessonsActivity;
 import com.example.apt_agile_piano_teaching.activities.LoginActivity;
 import com.example.apt_agile_piano_teaching.R;
 import com.example.apt_agile_piano_teaching.activities.RegistrationActivity;
 import com.example.apt_agile_piano_teaching.activities.StudentsActivity;
+import com.example.apt_agile_piano_teaching.models.Assignment;
+import com.example.apt_agile_piano_teaching.models.User;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -42,6 +52,11 @@ public class ProfileFragment extends Fragment {
     private Button mEditProfileBtn;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore mDbReference = FirebaseFirestore.getInstance();
+
+    private EditText userName;
+    private EditText userLastName;
+    private Button confirmButton;
+    private Button cancelButton;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -74,7 +89,7 @@ public class ProfileFragment extends Fragment {
         mEditProfileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), ImagesActivity.class));
+                showDialog();
             }
         });
 
@@ -100,5 +115,45 @@ public class ProfileFragment extends Fragment {
         });
 
         return view;
+    }
+
+    public void showDialog() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.edit_profile_dialog);
+
+        userName = dialog.findViewById(R.id.userName);
+        userLastName = dialog.findViewById(R.id.userLastName);
+        confirmButton = dialog.findViewById(R.id.userEditConfirm);
+        cancelButton = dialog.findViewById(R.id.userEditCancel);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (userName != null
+                && userLastName != null) {
+
+                    User user = new User(mAuth.getCurrentUser().getEmail(), userName.getText().toString(), userLastName.getText().toString());
+
+                    mDbReference.collection("users").document(mAuth.getCurrentUser().getUid())
+                            .set(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    dialog.dismiss();
+                                }
+                            });
+                }
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
