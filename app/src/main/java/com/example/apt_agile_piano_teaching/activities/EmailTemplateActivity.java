c9fc884a51025fc1e9f50a73d94747cf53790871
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2022-08-24 16:27:47 +0200

    add mail template filler

--- app/src/main/java/com/example/apt_agile_piano_teaching/activities/EmailTemplateActivity.java
+++ app/src/main/java/com/example/apt_agile_piano_teaching/activities/EmailTemplateActivity.java
@@ -8,8 +8,11 @@ import android.widget.Toast;
 
 import com.example.apt_agile_piano_teaching.databinding.ActivityEmailTemplateBinding;
 import com.example.apt_agile_piano_teaching.models.EmailTemplate;
+import com.example.apt_agile_piano_teaching.utils.Preference;
+import com.example.apt_agile_piano_teaching.utils.Settings;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EmailTemplateActivity extends AppCompatActivity {
@@ -23,6 +26,20 @@ public class EmailTemplateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityEmailTemplateBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Settings emailSettings = new Settings(EmailTemplateActivity.this, Preference.IMAGE_TEMPLATE_NOT_NULl);
+        if (emailSettings.getPreference()) {
+            mDbReference.collection("emailTemplates")
+                    .document(mAuth.getCurrentUser().getUid())
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            binding.mailSubject.setText(documentSnapshot.getString("subject"));
+                            binding.mailText.setText(documentSnapshot.getString("text"));
+                        }
+                    });
+        }
+
 
         binding.saveTemplateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +56,7 @@ public class EmailTemplateActivity extends AppCompatActivity {
 
     private void setupMail() {
         EmailTemplate emailTemplate = new EmailTemplate(binding.mailSubject.getText().toString(), binding.mailText.getText().toString());
+        Settings emailSettings = new Settings(EmailTemplateActivity.this, Preference.IMAGE_TEMPLATE_NOT_NULl);
 
         mDbReference.collection("emailTemplates")
                 .document(mAuth.getCurrentUser().getUid())
@@ -47,6 +65,7 @@ public class EmailTemplateActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(Void unused) {
                         Toast.makeText(EmailTemplateActivity.this, "Salvataggio del template avvenuto con successo!", Toast.LENGTH_SHORT).show();
+                        emailSettings.setTrue();
                     }
                 });
     }
