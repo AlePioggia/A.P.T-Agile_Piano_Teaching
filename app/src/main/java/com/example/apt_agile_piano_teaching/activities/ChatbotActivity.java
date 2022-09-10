bbcb303259c911357ab545e994e13c20e62e9eef
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2022-09-10 17:57:46 +0200

    fix visual chat bug

--- app/src/main/java/com/example/apt_agile_piano_teaching/activities/ChatbotActivity.java
+++ app/src/main/java/com/example/apt_agile_piano_teaching/activities/ChatbotActivity.java
@@ -35,6 +35,9 @@ public class ChatbotActivity extends AppCompatActivity {
         binding.chatRecyclerView.setLayoutManager(manager);
         binding.chatRecyclerView.setAdapter(adapter);
 
+        chats.add(new Chat("Ciao collega, sono a tua disposizione! Digita 'aiuto' per ottenere la lista di comandi!", BOT_KEY));
+        adapter.notifyDataSetChanged();
+
         binding.idFABSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,10 +60,10 @@ public class ChatbotActivity extends AppCompatActivity {
                     "la seconda si calcola sommando 3 semitoni alla prima e la terza 3 semitoni a partire dalla seconda", BOT_KEY));
             adapter.notifyDataSetChanged();
         } else if (message.toLowerCase().equals("ciao")) {
-            chats.add(new Chat("Ciao collega, sono a tua disposizione!", BOT_KEY));
+            chats.add(new Chat("Ciao collega, sono a tua disposizione! Digita 'aiuto' per ottenere la lista di comandi!", BOT_KEY));
             adapter.notifyDataSetChanged();
         } else if (message.toLowerCase().equals("accordo minore")) {
-            chats.add(new Chat("La triade di accordo maggiore è composta da 3 note, la prima è la nota di riferimento (ad esempio LA)"
+            chats.add(new Chat("La triade di accordo minore è composta da 3 note, la prima è la nota di riferimento (ad esempio LA)"
             + "la seconda si calcola sommando 2 semitoni alla prima e la terza 4 semitoni a partire dalla seconda", BOT_KEY));
             adapter.notifyDataSetChanged();
         } else if (message.toLowerCase().equals("semitono")) {
@@ -68,6 +71,17 @@ public class ChatbotActivity extends AppCompatActivity {
                     "equivalente alla metà di un tono; può essere diatonico (e corrisponde allora a un intervallo di seconda: per es. mi-fa ) " +
                     "o cromatico (quando unisce una nota alla sua alterazione: per es. do-do diesis ; do-do bemolle ).", BOT_KEY));
             adapter.notifyDataSetChanged();
+        } else if (message.toLowerCase().equals("scala")) {
+            chats.add(new Chat("Nella teoria musicale, una scala è una successione di suoni nell'ambito di un'ottava, di cui l'ultimo è una ripetizione " +
+                    "del primo esattamente un'ottava sopra." +
+                    " Si chiama scala ascendente una scala in cui l'altezza delle note cresce, e scala discendente una in cui l'ordine è decrescente." +
+                    " Una stessa scala può presentare alterazioni differenti se considerata nel tratto ascendente o in quello discendente.", BOT_KEY));
+        } else if (message.toLowerCase().equals("aiuto")) {
+            chats.add(new Chat("Digita i seguenti comandi per approfondire gli argomenti citati: \n" +
+                    "'accordo maggiore' \n" +
+                    "'accordo minore' \n" +
+                    "'semitono' \n"
+                    + "'scala'", BOT_KEY));
         }
         else {
             chats.add(new Chat("Mi spiace collega, non sono in grado di decifrare il messaggio che hai inviato, digita help per avere informazioni", BOT_KEY));
